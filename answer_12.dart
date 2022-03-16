aa49e33c1a8d882d5f11b53ac45e1fdae51f913e
Masum <masumrahman099@gmail.com>
2022-03-17 00:53:45 +0600

    some changes of answer 12

--- answer_12.dart
+++ answer_12.dart
@@ -1,10 +1,17 @@
 // Answer to the question number 12:
 //Problem solving
 
-void main(){
+void main() {
+  int number0 = 4;
+  int number1 = 9;
 
-  var num = {4,9};
-  var result = num == 9? "It's even" : "It's odd";
-  print(result);
+  if (number0 % 2 == 0) {
+    print("number0 is even number");
+  } else 
+  print("number0 is odd number");
 
-  } 
+  if (number1 % 2 == 1) {
+    print("number1 is odd number");
+  } else 
+  print("number1 is even number");
+}
